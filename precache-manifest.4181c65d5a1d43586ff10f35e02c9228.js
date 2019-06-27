b64225aa06b8c92d73477fca3a4e5e4a949697b0
Sanam J <sanam.javidnia@gmail.com>
2019-06-27 13:39:37 -0700

    first commit

---
+++ precache-manifest.4181c65d5a1d43586ff10f35e02c9228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f133971748def6db88a7db32c9aee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9052af8bd9118fb5c4f4",
+    "url": "/static/css/main.6b6bd93a.chunk.css"
+  },
+  {
+    "revision": "71c127a36f353fe13730",
+    "url": "/static/js/2.7536bc24.chunk.js"
+  },
+  {
+    "revision": "9052af8bd9118fb5c4f4",
+    "url": "/static/js/main.12818394.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
